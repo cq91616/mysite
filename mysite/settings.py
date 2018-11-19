f772346a9aab840d981891291de0c25f9aabd838
Terrence <lanyuan_cq@163.com>
2018-11-19 17:10:53 +0800

    base.html + bootstrap

--- mysite/settings.py
+++ mysite/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.static'
+                #'django.template.context_processors.static'
             ],
         },
     },
@@ -129,5 +129,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATICFILES_DIRS=(os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT =(os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS=(os.path.join(BASE_DIR, "static"),)
+#STATIC_ROOT =(os.path.join(BASE_DIR, "static"),)
+
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
+)
