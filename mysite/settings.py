6953961efe601f7d99c4615f649a52636c7fc03e
Terrence <lanyuan_cq@163.com>
2018-11-19 17:32:20 +0800

    base.html + bootstrap

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=(os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS=(os.path.join(BASE_DIR, "static"),
+                  '/opt/mysite/static/')
 #STATIC_ROOT =(os.path.join(BASE_DIR, "static"),)
 
 # STATICFILES_FINDERS = (
