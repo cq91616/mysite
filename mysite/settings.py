a203c1962e7ea0b8a5d7a2319a2ba820143d6a3c
Terrence <lanyuan_cq@163.com>
2018-11-24 11:25:51 +0800

    top & bottom

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=(os.path.join(BASE_DIR, "static"),
-                  '/opt/mysite/static/')
+# STATICFILES_DIRS=(os.path.join(BASE_DIR, "static"),
+#                   '/opt/mysite/static/')
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
